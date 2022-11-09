6974adb259ac92492304837dff6a075cdb4ef27f
Daniel Tan <dtch1997@users.noreply.github.com>
2022-11-09 11:45:25 +0000

    Add RL algorithm, update to Gymnasium interface

--- latent_landmarks/envs/__init__.py
+++ latent_landmarks/envs/__init__.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 from gymnasium.envs.registration import register
 
 register(
-    id='GoalA1Env-v1',
+    id='GoalA1-v1',
     entry_point='latent_landmarks.envs.goal_unitree_a1:GoalA1Env',
     max_episode_steps=100,
 )
--- tests/test_goal_a1_env.py
+++ tests/test_goal_a1_env.py
@@ -2,11 +2,11 @@ import gymnasium as gym
 import latent_landmarks.envs # Register envs
 
 def test_reset():
-    env = gym.make("GoalA1Env-v1")
+    env = gym.make("GoalA1-v1")
     env.reset(seed=42)
 
 def test_obs_space():
-    env = gym.make("GoalA1Env-v1")
+    env = gym.make("GoalA1-v1")
     observation, info = env.reset(seed=42)
     assert observation['observation'].shape == (35,)
     assert observation['desired_goal'].shape == (12,)
